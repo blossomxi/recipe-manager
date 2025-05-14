df7cae7ebd93ac41a0ac394a217352ac39471262
Damian Perez <damedash03@gmail.com>
2025-05-14 14:59:42 -0700

    refactor: Remove redundant directives from DietType.h and MealType.h

--- src/DietType.h
+++ src/DietType.h
@@ -1,6 +1,5 @@
 // src/DietType.h
 // Defines the DietType enum to categorize recipes by dietary restriction.
-#pragma once
 
 enum class DietType {
     Vegetarian,
--- src/MealType.h
+++ src/MealType.h
@@ -1,6 +1,5 @@
 // src/MealType.h
 // Defines the MealType enum to categorize recipes by meal.
-#pragma once
 
 enum class MealType {
     Breakfast,
